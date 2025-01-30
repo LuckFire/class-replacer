50e9636eaa2b0106a09b3241a33b73d11cf86ef6
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-01-30 18:09:28 -0500

    init

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env bun
+
+import sade from 'sade';
+import { getScssFiles, readFile, writeFile, replaceClasses } from './util';
+
+const prog = sade('class-replacer');
+
+prog
+    .command('run')
+    .describe('Run the class replacer')
+    .option('--path, -p', 'The path that includes all of your css/scss files. Root is the directory that the cli command was ran in. Default path is \`src\`')
+    .option('--ignore, -i', 'Ignore certain files when replacing classes. Separate multiple by commas.')
+    .action(async ({ path, ignore }) => {
+        const files = getScssFiles(
+            path || 'src',
+            ignore !== undefined ? ignore.split(',') : [],
+        );
+
+        console.log(files);
+
+        for (const file of files) {
+            const scss = readFile(file);
+            const lines = scss.split('\n');
+
+            const updatedScss = replaceClasses(lines);
+            writeFile(file, updatedScss.join('\n'));
+        }
+    });
+
+prog.parse(process.argv);
---
+++ src/util.ts
@@ -0,0 +1,62 @@
+import path from "path";
+import { readFileSync, writeFileSync, readdirSync } from "fs";
+
+const classes: Record<string, string> = JSON.parse(readFileSync(path.join(__dirname, "class-mapping.json"), "utf8"));
+
+export function getScssFiles(basePath?: string, ignoreList: string[] = []) {
+    const root = process.cwd();
+
+    const files = readdirSync(
+        path.join(root, basePath || ''),
+        { recursive: true, withFileTypes: true },
+    );
+
+    const scssFiles = files
+        .filter((file) => file.isFile())
+        .filter((file) => !ignoreList.includes(file.name))
+        .filter((file) => 
+            file.name.endsWith('.scss') || file.name.endsWith('.css')
+        );
+
+    return scssFiles.map((file) => path.join(file.parentPath, file.name));
+}
+
+export function readFile(path: string) {
+    return readFileSync(path, "utf8");
+}
+
+export function writeFile(path: string, data: string) {
+    return writeFileSync(path, data, "utf8");
+}
+
+export function replaceClasses(lines: string[]) {
+    const regex = new RegExp(/(?<class_name>(?<=\.)[A-Za-z0-9\-\_]+)/gm)
+
+    for (let i = 0; i < lines.length; i++) {
+        let line = lines[i];
+        const matches = [ ...line.matchAll(regex) ];
+
+        if (!matches || !matches.length) continue;
+
+        let isMatching = false;
+        for (const match of matches) {
+            if (!match.groups) continue;
+
+            const groups = match.groups as { class_name: string };
+            const className = groups.class_name;
+
+            if (!classes[className]) continue;
+            isMatching = true;
+
+            line = line.replace(
+                new RegExp(`\\b${className}\\b`, 'g'),
+                classes[className]
+            );
+        }
+
+        if (!isMatching) continue;
+        lines[i] = line;
+    }
+
+    return lines;
+}
